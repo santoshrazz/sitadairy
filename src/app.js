de9c5349c7fc6230fe218ceaab6ceed074b5b334
santoshrazz <santoshrajbgp11@gmail.com>
2025-06-25 23:55:53 +0530

    third trial commit

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
     res.send("Hello From Santosh's Api")
 })
 app.get("/try", (req, res) => {
-    res.send("Hello This is trying route for the")
+    res.send("Hello This is trying route for the sonarqube added new line")
 })
 app.use('/api/v1/user', userRoute)
 app.use('/api/v1/milk', milkRouter)
