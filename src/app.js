3050dca5d27f574bb46e5abf81c84eeb4601d616
Santosh Kumar <119107741+santoshrazz@users.noreply.github.com>
2026-01-23 23:41:02 +0530

    Testing Latest deployment on self hosted runner

--- src/app.js
+++ src/app.js
@@ -62,7 +62,7 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 app.get("/", (req, res) => {
-  res.send("Hello From Santosh's Api And Latest Image have been pushed And Github Action Runs");
+  res.send("Hello From Santosh's Api And Latest Image have been pushed And Github Action Runs And Again Testing the deployment");
 });
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/milk", milkRouter);
